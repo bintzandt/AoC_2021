88d5f7cbcdbd664711fc4f8d7162595e73176bf1
Bram in 't Zandt <bram@bigspark.com>
2021-12-01 19:50:35 +0100

    ♻ improved solution for day 1

--- Day1/Main.hs
+++ Day1/Main.hs
@@ -7,17 +7,11 @@ import AOC
 
 task1 = do
 	input <- readAsListOfInt "Day1/input.txt"
-	print $ calcIncrements input
+	print $ calcIncrements 1 input
 
 task2 = do
 	input <- readAsListOfInt "Day1/input.txt"
-	print $ calcIncrements $ map sum $ partition input []
+	print $ calcIncrements 3 input
 
-calcIncrements :: [Int] -> Int
-calcIncrements input = snd $ foldl (\(prev, count) curr -> if curr > prev then (curr, count+1) else (curr, count)) (head input, 0) (tail input)
-
-partition :: [Int] -> [[Int]] -> [[Int]]
-partition [] result = result
-partition (x:[]) result = result
-partition (x:y:[]) result = result
-partition input result = partition (drop 1 input) (result ++ [(take 3 input)])
+calcIncrements :: Int -> [Int] -> Int
+calcIncrements increment input = sum $ zipWith (\x y -> if x < y then 1 else 0) input $ drop increment input
