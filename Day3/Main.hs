c4492cd0f9d8d659888a79650ba9bbcc9cee3b0c
Bram in 't Zandt <bram@bigspark.com>
2021-12-03 11:34:04 +0100

    💩 code for Day 3

---
+++ Day3/Main.hs
@@ -0,0 +1,47 @@
+module Day3.Main (
+	task1,
+	task2,
+) where
+
+import AOC
+import Data.List
+import Data.Char
+
+task1 = do
+	input <- readAsListOfString "Day3/input.txt"
+	let lengthOfInput = length input
+	let (gamma, epsilon) = calculateRates lengthOfInput $ map sum $ map convertStringToListOfInt $ transpose input
+	print (gamma * epsilon)
+
+convertStringToListOfInt :: String -> [Int]
+convertStringToListOfInt = map digitToInt
+
+
+
+calculateRates :: Int -> [Int] -> (Int, Int)
+calculateRates size list = (binToDec $ map (\x -> if 2 * x > size then 1 else 0) list, binToDec $ map (\x -> if 2 * x > size then 0 else 1) list)
+
+binToDec l = sum $ map (2^) $ findIndices (==1) $ reverse l
+
+oxygenGeneratorRating :: [String] -> Int -> String
+oxygenGeneratorRating (x:[]) _ = x
+oxygenGeneratorRating xs i = oxygenGeneratorRating (filter (\x -> (x !! i) == keep) xs) (i+1)
+	where
+		transposed = transpose xs
+		size = length xs
+		keep = if (2 * ((map sum $ map convertStringToListOfInt $ transposed) !! i)) >= size then '1' else '0'
+
+cO2ScrubberRating :: [String] -> Int -> String
+cO2ScrubberRating (x:[]) _ = x
+cO2ScrubberRating xs i = cO2ScrubberRating (filter (\x -> (x !! i) == keep) xs) (i+1)
+	where
+		transposed = transpose xs
+		size = length xs
+		keep = if (2 * ((map sum $ map convertStringToListOfInt $ transposed) !! i)) < size then '1' else '0'
+
+task2 = do
+	input <- readAsListOfString "Day3/input.txt"
+	let oxygen = oxygenGeneratorRating input 0
+	let co2 = cO2ScrubberRating input 0
+	print oxygen
+	print co2
--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 import Day1.Main as Day1
 import Day2.Main as Day2
+import Day3.Main as Day3
 
 import AOC
